34882e3450a6a708aad96c78d66f08287bd4edaf
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-18 23:00:47 +0100

    Update gitPush.js

--- src/web/subdomains/api/routes/gitPush.js
+++ src/web/subdomains/api/routes/gitPush.js
@@ -2,6 +2,8 @@ module.exports = {
     path: "/git/push",
     method: "GET",
     init: (req, res) => {
+
+        console.log(req.method);
         res.send("GIT PUSH RECEIVED!");
 
         console.log(req.body);
