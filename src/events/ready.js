3ede49831801670c29410f9b7f1a782b6264ddbb
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-19 00:31:52 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ const { Events } = require('discord.js');
 module.exports = {
     event: Events.ClientReady,
     handle(c) {
-        console.log(`Ready super! Logged in as ${c.user.tag}`);
+        console.log(`Ready! Logged in as ${c.user.tag}`);
     }
 }
