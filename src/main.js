18456a6e58746fc7e9b060f6c3a04a693347fd71
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-19 20:47:42 +0100

    Removed unused import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
