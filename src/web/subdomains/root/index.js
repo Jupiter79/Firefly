8e7101aaa21a920d67e52e93eedd1ffa6a5c3f53
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-19 01:55:23 +0100

    Stupid mistake

--- src/web/subdomains/root/index.js
+++ src/web/subdomains/root/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 module.exports = () => {
     var router = express.Router();
 
-    app.all('/*', function(req, res, next) {
+    router.all('/*', function(req, res, next) {
         res.sendFile('index.html', { root: "web_html" });
     });
 
