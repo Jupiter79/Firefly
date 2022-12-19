20b41301e6ec693cbf467ecd31d9d3ac2bcc5244
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-19 01:57:27 +0100

    Reverted 2 commits

--- src/web/subdomains/root/index.js
+++ src/web/subdomains/root/index.js
@@ -3,7 +3,9 @@ var express = require('express');
 module.exports = () => {
     var router = express.Router();
 
-    router.use("/", express.static("web_html"));
+    router.all('/*', function(req, res, next) {
+        res.sendFile('index.html', { root: "web_html" });
+    });
 
     return router;
 }
