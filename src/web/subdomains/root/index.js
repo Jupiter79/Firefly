af6aee150d0e3b94ccb53be185c3e3672e32a75c
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-19 01:57:59 +0100

    Fixed

--- src/web/subdomains/root/index.js
+++ src/web/subdomains/root/index.js
@@ -3,9 +3,7 @@ var express = require('express');
 module.exports = () => {
     var router = express.Router();
 
-    router.all('/*', function(req, res, next) {
-        res.sendFile('index.html', { root: "web_html" });
-    });
+    router.use("/", express.static("web_html"));
 
     return router;
 }
