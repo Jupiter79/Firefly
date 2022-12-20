88bfc18a5d1bd76b42c4882c6168cb635712fd0e
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-20 17:00:58 +0100

    Added important commands at startup

--- src/web/subdomains/api/routes/gitPush.js
+++ src/web/subdomains/api/routes/gitPush.js
@@ -16,7 +16,7 @@ module.exports = {
         if (validate_secret(req.headers["x-hub-signature"], req.body) && req.body.ref == "refs/heads/main") {
             res.json({ code: 200, msg: "OK" });
 
-            exec("git pull && pm2 restart Firefly");
+            exec("git pull && npm install && npx prisma migrate dev && pm2 restart Firefly");
         } else res.json({code: 400, msg: "Not authorized"});
     }
 }
