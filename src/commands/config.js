a3acfc315f4f7b490d664d4be9a1a4d6a15b7d69
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-20 20:45:45 +0100

    Config welcome improve Fixes #16

--- src/commands/config.js
+++ src/commands/config.js
@@ -9,44 +9,42 @@ module.exports = {
         .setDescription("Change the config for this server")
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
         .setDMPermission(false)
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName("welcome")
+        .addSubcommandGroup(subcommandgroup =>
+            subcommandgroup.
+                setName("welcome")
                 .setDescription("Define a welcome-channel in which new users should be greeted")
-                .addChannelOption(option =>
-                    option
-                        .setName("channel")
-                        .setDescription("DEFINE a welcome channel")
-                        .addChannelTypes(ChannelType.GuildText)
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName("view")
+                        .setDescription("Shows the current welcome channel")
                 )
-                .addBooleanOption(option =>
-                    option
-                        .setName("delete")
-                        .setDescription("DELETE the current welcome channel (if defined)")
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName("define")
+                        .setDescription("Define a channel in which new users should be greeted")
+                        .addChannelOption(option =>
+                            option
+                                .setName("channel")
+                                .setDescription("The channel in which new users should be greeted")
+                                .addChannelTypes(ChannelType.GuildText)
+                        )
+                )
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName("reset")
+                        .setDescription("Resets the welcome channel so new users won't be greeted anymore")
                 )
         ),
     async execute(interaction) {
+        var subcommandgroup = interaction.options.getSubcommandGroup();
         var subcommand = interaction.options.getSubcommand();
 
-        if (subcommand == "welcome") {
-            var channel = interaction.options.getChannel("channel");
-            var _delete = interaction.options.getBoolean("delete");
+        var guild = await prisma.guild.findUnique({
+            where: { id: interaction.guild.id }
+        })
 
-            var guild = await prisma.guild.findUnique({
-                where: { id: interaction.guild.id }
-            })
-
-            if (channel) {
-                if (channel.id == guild.welcome_channel) return await interaction.reply({ content: "This is already the welcome channel!", ephemeral: true });
-
-                await prisma.guild.update({ where: { id: interaction.guild.id }, data: { welcome_channel: channel.id } });
-                await interaction.reply(`The welcome-channel has been successfully set to ${channel.toString()}`)
-            } else if (_delete) {
-                if (!guild?.welcome_channel) return await interaction.reply({ content: "There hasn't been defined a welcome-channel yet!", ephemeral: true });
-
-                await prisma.guild.update({ where: { id: interaction.guild.id }, data: { welcome_channel: null } });
-                await interaction.reply("The welcome-channel has been successfully deleted!");
-            } else {
+        if (subcommandgroup == "welcome") {
+            if (subcommand == "view") {
                 if (!guild?.welcome_channel) return await interaction.reply("There's no defined welcome channel")
 
                 interaction.guild.channels.fetch(guild.welcome_channel)
@@ -57,6 +55,18 @@ module.exports = {
                         await prisma.guild.update({ where: { id: interaction.guild.id }, data: { welcome_channel: null } });
                         await interaction.reply("There's no defined welcome channel")
                     });
+            } else if (subcommand == "define") {
+                var channel = interaction.options.getChannel("channel");
+
+                if (channel.id == guild.welcome_channel) return await interaction.reply({ content: "This is already the welcome channel!", ephemeral: true });
+
+                await prisma.guild.update({ where: { id: interaction.guild.id }, data: { welcome_channel: channel.id } });
+                await interaction.reply(`The welcome-channel has been successfully set to ${channel.toString()}`)
+            } else if (subcommand == "reset") {
+                if (!guild?.welcome_channel) return await interaction.reply({ content: "There hasn't been defined a welcome-channel yet!", ephemeral: true });
+
+                await prisma.guild.update({ where: { id: interaction.guild.id }, data: { welcome_channel: null } });
+                await interaction.reply("The welcome-channel has been successfully deleted!");
             }
         }
     },
