34a06232b55375dd925af055a8d80fb552ae1a8d
Sergio Huainigg <94369932+Jupiter79@users.noreply.github.com>
2022-12-22 00:11:37 +0100

    Critical language problem fixed

--- src/lang/index.js
+++ src/lang/index.js
@@ -18,12 +18,14 @@ global.LANGUAGES.list.filter(x => x != "default").forEach(lang => {
 
     commands.filter(x => Object.keys(x[1].meta).length > 0).forEach(command => {
         var metas = Object.entries(command[1].meta);
-        global.COMMAND_META[command[0]] = {}
+        if (!global.COMMAND_META[command[0]]) global.COMMAND_META[command[0]] = {}
 
         metas.filter(x => x.length > 0).forEach(meta => {
-            global.COMMAND_META[command[0]][meta[0]] = {};
+            if (!global.COMMAND_META[command[0]][meta[0]]) global.COMMAND_META[command[0]][meta[0]] = {};
 
             global.COMMAND_META[command[0]][meta[0]][lang] = meta[1];
         })
     })
 })
+
+console.log(global.COMMAND_META)
